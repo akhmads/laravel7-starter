04671004847add2c89fb3bf849ef84a1829d296c
Akhmad <akhmadshaleh@gmail.com>
2023-07-04 21:46:03 +0700

    add theme

---
+++ resources/views/layouts/partials/script.blade.php
@@ -0,0 +1,17 @@
+    
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+<script src="{{ url('assets/vendor/libs/jquery/jquery.js') }}"></script>
+<script src="{{ url('assets/vendor/libs/popper/popper.js') }}"></script>
+<script src="{{ url('assets/vendor/js/bootstrap.js') }}"></script>
+<script src="{{ url('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+<script src="{{ url('assets/vendor/libs/hammer/hammer.js') }}"></script>
+<script src="{{ url('assets/vendor/js/menu.js') }}"></script>
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+
+<!-- Main JS -->
+<script src="{{ url('assets/js/main.js') }}"></script>
+
+<!-- Page JS -->
