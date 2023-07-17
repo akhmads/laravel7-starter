766ed8cfff16530404059f5c3f0b8421b811bcd7
Akhmad <akhmadshaleh@gmail.com>
2023-07-17 21:23:07 +0700

    add login page

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html
+  lang="en"
+  class="light-style layout-navbar-fixed layout-menu-fixed"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="{{ url('assets') }}/"
+  data-template="vertical-menu-template-starter"
+>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
+  <!-- Core head -->
+  @include('admin.partials.head', ['vendor' => isset($vendor)?$vendor:[]])
+  
+  <!-- head stack -->
+  @stack('head')
+
+  <link rel="stylesheet" href="{{ url('assets/vendor/css/pages/page-auth.css') }}" />
+
+  <title>App Name - @yield('title')</title>
+</head>
+<body>
+
+<!-- Content -->
+<div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Register -->
+        <div class="card">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <a href="index.html" class="app-brand-link gap-2">
+                <span class="app-brand-logo demo">
+                    @include('admin.partials.logo')
+                </span>
+                <span class="app-brand-text demo text-body fw-bolder">Hypercode</span>
+              </a>
+            </div>
+            <!-- /Logo -->
+            {{-- <h4 class="mb-2">Welcome to Sneat! 👋</h4>
+            <p class="mb-4">Please sign-in to your account and start the adventure</p> --}}
+
+            <form class="mb-3" action="{{ url('/login') }}" method="POST">
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" value="{{ old('email') }}" class="form-control" name="email-username" placeholder="Enter your email or username" autofocus />
+              </div>
+              <div class="mb-3 form-password-toggle">
+                <div class="d-flex justify-content-between">
+                  <label class="form-label" for="password">Password</label>
+                  {{-- <a href="auth-forgot-password-basic.html">
+                    <small>Forgot Password?</small>
+                  </a> --}}
+                </div>
+                <div class="input-group input-group-merge">
+                  <input type="password" class="form-control" name="password" id="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                  <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                </div>
+              </div>
+              <div class="mb-3">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" name="remember-me" id="remember-me" />
+                  <label class="form-check-label" for="remember-me"> Remember Me </label>
+                </div>
+              </div>
+              <div class="mb-3">
+                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+              </div>
+            </form>
+
+            <p class="text-center">
+              <span>New on our platform?</span>
+              <a href="auth-register-basic.html">
+                <span>Create an account</span>
+              </a>
+            </p>
+
+          </div>
+        </div>
+        <!-- /Register -->
+      </div>
+    </div>
+  </div>
+
+  <!-- / Content -->
